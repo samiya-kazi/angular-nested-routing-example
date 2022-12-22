b1b9b804fc9ea6dfd686247db6d31582901550e7
Samiya Kazi <samiya.kazi09@gmail.com>
2022-12-22 16:56:11 +0600

    added comments

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -6,6 +6,9 @@ import { ProductComponent } from './product/product.component';
 import { ProductDetailsComponent } from './product-details/product-details.component';
 import { ProductReviewsComponent } from './product-reviews/product-reviews.component';
 
+// The rested routes are added to the children property of their parent route.
+// The paths for the children are relative to the path of their parent.
+
 const routes: Routes = [
   {path: '', component: HomeComponent, pathMatch:'full'},
   {path: 'product', component: ProductComponent, 
